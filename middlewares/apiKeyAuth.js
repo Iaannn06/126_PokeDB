d2933d9ecb804a18b41436ddf647ef5e04bacff2
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-01-12 07:55:25 +0700

    menambahkan try dan catch untuk api key

--- middlewares/apiKeyAuth.js
+++ middlewares/apiKeyAuth.js
@@ -7,5 +7,17 @@ const validateApiKey = async (apiKey) => {
         return res.status(401).json({ message: 'Akses Ditolak, API Key tidak ditemukan.' });
     }
 
-    
+    try {
+       const [rows] = await db.query('SELECT * FROM api_keys WHERE key_string = ? AND is_active = 1', [apiKey]);
+        
+        if (rows.length > 0) {
+            next(); 
+        } else {
+            res.status(403).json({ error: "API Key tidak valid." });
+        }
+    }catch (error) {
+        res.status(500).json({ error: "Server Error saat validasi key." });
+    }
+
+
 }
