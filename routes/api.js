ed8410c7da8687b8921ea70f6e684b94e7a7e957
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-01-12 11:44:55 +0700

    penyesuaian pada api.js

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
-const pokeController = require('../controllers/pokemonController');
 const validateKey = require('../middlewares/apiKeyAuth');
+const authController = require('../controllers/authController');
+const pokemonController = require('../controllers/pokemonController');
 
-router.post('/auth/register', authController.register); // <-- Register
-router.post('/auth/login', authController.login);       // <-- Login
+router.post('/auth/register', authController.register);
+router.post('/auth/login', authController.login);
 router.post('/dashboard/generate-key', authController.generateKey);
 router.get('/dashboard/my-key', authController.getMyKey);
-
-router.get('/v1/pokemon/:name', validateKey, pokeController.getGlobalPokemon);
-router.post('/v1/pokemon', validateKey, pokeController.createCustomPokemon);
-router.get('/v1/custom-pokemon', validateKey, pokeController.getCustomPokemon);
+router.get('/v1/pokemon/:name', validateKey, pokemonController.getGlobalPokemon);
+router.post('/v1/pokemon', validateKey, pokemonController.createCustomPokemon);
+router.get('/v1/custom-pokemon', validateKey, pokemonController.getCustomPokemon);
+router.delete('/v1/pokemon/:id', validateKey, pokemonController.deleteCustomPokemon);
+router.put('/v1/pokemon/:id', validateKey, pokemonController.updateCustomPokemon);
 
 module.exports = router;
