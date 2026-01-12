a4df6b7a5e7807a1d610cea961fd81bdbc675b92
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-01-12 08:00:29 +0700

    menambahkan try dan catch error untuk mengecek akun

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,5 +5,13 @@ exports.register = async (req, res) => {
     const { email, password, company_name } = req.body;
     if (!email || !password || !company_name) return res.status(400).json({ error: "Data tidak lengkap!" });
 
-    
+    try {
+        const [exist] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
+        if (exist.length > 0) return res.status(400).json({ error: "Email sudah terdaftar!" });
+
+        await db.query('INSERT INTO users (email, password, company_name) VALUES (?, ?, ?)', [email, password, company_name]);
+        res.json({ status: 'success', message: 'Registrasi Berhasil! Silakan Login.' });
+    } catch (error) {
+        res.status(500).json(error);
+    }
 };
