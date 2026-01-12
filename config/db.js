7bf26db51f2f152a7ffd2f5059d257fdb72eef59
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-01-12 09:55:11 +0700

    perbaikan pada db.js

--- config/db.js
+++ config/db.js
@@ -1,22 +1,25 @@
 const mysql = require('mysql2');
 
+require('dotenv').config(); 
+
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: process.env.PORT,
     waitForConnections: true,
     connectionLimit: 10,
 });
 
-pool.getConnection()
+
+pool.promise().getConnection()
     .then(conn => {
-        console.log('Database connected successfully.');
+        console.log('✅ Database connected successfully.');
         conn.release(); 
     })
     .catch(err => {
-        console.error('Error connecting to the database:', err);
+        console.error('❌ Error connecting to the database:', err.message);
     });
 
+// Export versi promise-nya biar bisa dipake di controller dengan async/await
 module.exports = pool.promise();
